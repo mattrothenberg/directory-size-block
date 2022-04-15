e1046166e696c3fb4d31f8eb84dd3fa85124cedf
Matt Rothenberg <mattrothenberg@github.com>
2022-04-14 20:01:12 -0400

    fix: fill-rule -> fillRule

--- src/blocks/example-folder-block/timeline.tsx
+++ src/blocks/example-folder-block/timeline.tsx
@@ -55,7 +55,7 @@ export const Timeline = ({
               stroke="currentColor"
             ></path>
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"
             ></path>
           </svg>
@@ -63,7 +63,7 @@ export const Timeline = ({
           <svg viewBox="0 0 24 24" width="24" height="24">
             <path d="M9.5 15.584V8.416a.5.5 0 01.77-.42l5.576 3.583a.5.5 0 010 .842l-5.576 3.584a.5.5 0 01-.77-.42z"></path>
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"
             ></path>
           </svg>
