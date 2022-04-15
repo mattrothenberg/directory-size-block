f2bfaae46375af42874d7dc06eed8bb4fe202857
Matt Rothenberg <mattrothenberg@github.com>
2022-04-14 20:23:48 -0400

    use grid

--- src/blocks/example-folder-block/bars.tsx
+++ src/blocks/example-folder-block/bars.tsx
@@ -23,32 +23,39 @@ export const Bars = ({
 
   return (
     <LayoutGroup>
-      <div className="relative z-0 p-4 space-y-1 mt-6">
+      <div className="relative z-0 px-4 pb-4 space-y-1 mt-6">
         {sortedTree.map(({ path, size }) => {
           const percent = ((size || 0) * 100) / maxSize;
           const beforeSize = beforeTree?.find((d) => d.path === path)?.size;
           const increased =
-            typeof size !== "undefined" && beforeSize && beforeSize < size;
+            typeof size !== "undefined" &&
+            Boolean(beforeSize && beforeSize < size);
           const decreased =
-            typeof size !== "undefined" && beforeSize && beforeSize > size;
+            typeof size !== "undefined" &&
+            Boolean(beforeSize && beforeSize > size);
 
           return (
             <motion.div
               layout="position"
               layoutId={path}
-              className="flex w-full items-center"
+              className="grid grid-cols-[100px_30px_240px_1fr] gap-2"
               key={path}
             >
-              <div className="flex-none w-16 max-w-[15%] truncate font-mono text-xs text-right mr-3 text-gray-500 flex items-center relative gap-1">
-                {formatSize(size)}
-                {increased && <ArrowUpRightIcon className="text-green-600" />}
-                {decreased && <ArrowDownLeftIcon className="text-red-600" />}
+              <div className="text-right text-gray-600">
+                <span className="font-mono text-xs">{formatSize(size)}</span>
               </div>
-              <div className="flex-none w-72 max-w-[20%] truncate font-mono text-xs mr-2">
-                {path}
+              <div className="text-center">
+                <span>
+                  {increased && <ArrowUpRightIcon className="text-green-600" />}
+                  {decreased && <ArrowDownLeftIcon className="text-red-600" />}
+                </span>
               </div>
-              <div className="flex-[3] relative min-w-0">
+              <div className="truncate text-gray-600">
+                <span className="font-mono text-xs">{path}</span>
+              </div>
+              <div>
                 <motion.div
+                  layout="position"
                   className="bg-indigo-600 h-4 relative"
                   initial={{
                     width: 0,
@@ -58,6 +65,27 @@ export const Bars = ({
                   }}
                 ></motion.div>
               </div>
+              {/* <motion.div className="flex-none truncate font-mono text-xs text-right text-gray-500">
+                {formatSize(size)}
+              </motion.div>
+              <motion.div className="w-4 flex-shrink-0">
+                {increased && <ArrowUpRightIcon className="text-green-600" />}
+                {decreased && <ArrowDownLeftIcon className="text-red-600" />}
+              </motion.div>
+              <motion.div className="flex-none truncate font-mono text-xs mr-2">
+                {path}
+              </motion.div>
+              <motion.div className="flex-1 relative min-w-0">
+                <motion.div
+                  className="bg-indigo-600 h-4 relative"
+                  initial={{
+                    width: 0,
+                  }}
+                  animate={{
+                    width: `${percent}%`,
+                  }}
+                ></motion.div>
+              </motion.div> */}
             </motion.div>
           );
         })}
